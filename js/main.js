880754e8cf4045519fd180c139a06bfec5b5a9b9
rolz <zrolland@gmail.com>
2014-05-13 16:05:29 +0200

    added homepage bg

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $( document ).ready(function() {
+
   $("#arrow-down").click(function() {
       $('html, body').animate({
           scrollTop: $("#jobb-form").offset().top
       }, 1000);
   });
+
 });
